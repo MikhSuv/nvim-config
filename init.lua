8a7a6e8250681bc17842432ed45a1579f5b100be
MikhSuv <mischasuv@mail.ru>
2025-05-08 19:52:47 +0300

    Rewritten using lazy.nvim and Mason

--- init.lua
+++ init.lua
@@ -1,245 +1,233 @@
+-- init.lua
+vim.g.mapleader = ',' -- Leader key
 
-vim.opt.mouse = "a" -- Enable mouse support
-vim.opt.encoding = "utf-8"       -- Общая кодировка (необязательно, по умолчанию UTF-8)
-vim.opt.fileencoding = "utf-8"  -- Кодировка файлов
-vim.opt.number = true -- Show line numbers
-vim.opt.cursorline = false -- Disable highlight current cursor's line
-vim.opt.swapfile = false -- Disable .swp files 
-vim.opt.scrolloff = 7 -- Number of lines left visible above/below the cursor when scrolling
-vim.opt.tabstop = 4 -- Spaces instead of one tab
-vim.opt.softtabstop = 4 -- Spaces instead of one tab
-vim.opt.shiftwidth = 4 -- Spaces for auto indent
-vim.opt.expandtab = true -- Replace tab with spaces
-vim.opt.autoindent = true -- Save indent on new line
+-- Настройки Neovim (оставить без изменений)
+vim.opt.mouse = "a"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.number = true
+vim.opt.cursorline = false
+vim.opt.swapfile = false
+vim.opt.scrolloff = 7
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
 vim.opt.fileformat = "unix"
 vim.opt.smartindent = true
-vim.opt.splitbelow = true -- horizontal split open below and right
+vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.g.mapleader = ',' -- Leader key
-vim.opt.termguicolors = true -- 24-bit colors
+vim.opt.termguicolors = true
 
--- Keymaps for programming languages
-vim.api.nvim_create_autocmd('FileType', {
+-- Lazy.nvim setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-    pattern = 'python',
-    callback = function()
+require("lazy").setup({
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    event = "BufReadPre",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+  },
+  
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }),
+      })
+    end,
+  },
 
-        vim.opt.colorcolumn = '88'
-        vim.keymap.set('n', '<C-h>', ':w<CR>:!python3.12 %<CR>', { buffer = true, silent = true })
-        vim.keymap.set('i', '<C-h>', '<Esc>:w<CR>:!python3.12 %<CR>', { buffer = true, silent = true })
-    end
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "cpp", "python", "fortran", "bash", "go" },
+        highlight = { enable = true },
+      })
+    end,
+  },
+
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-fzf-native.nvim",
+    },
+    config = function()
+      require("telescope").setup()
+      require("telescope").load_extension("fzf")
+    end,
+  },
+
+  -- Color schemes
+  { "morhetz/gruvbox" },
+  { "ayu-theme/ayu-vim" },
+  { "rebelot/kanagawa.nvim" },
+  { "sainnhe/gruvbox-material" },
+
+  -- Комментарии
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup({
+        padding = true,
+        toggler = { line = ",cc", block = ",cb" },
+        opleader = { line = ",c", block = ",b" },
+      })
+    end,
+  },
+
+  -- Автосохранение
+  {
+    "Pocco81/auto-save.nvim",
+    config = function()
+      require("auto-save").setup()
+    end,
+  },
+
+  -- Форматирование
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = { null_ls.builtins.formatting.prettier },
+      })
+    end,
+  },
+
+  -- Дополнительные LSP серверы
+  {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      -- Автоматическая установка не требуется, можно установить вручную через :Mason
+      automatic_installation = false,
+      
+      -- Базовые настройки для всех LSP-серверов
+      handlers = {
+        -- Дефолтный обработчик для всех LSP
+        function(server_name)
+          require("lspconfig")[server_name].setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            on_attach = function(client, bufnr)
+              -- Общие LSP keymaps
+              local opts = { buffer = bufnr, noremap = true, silent = true }
+              vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+              vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+              vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+              vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+            end
+          })
+        end,
+        
+        -- Специфичные настройки для отдельных LSP
+        ["pyright"] = function()
+          require("lspconfig").pyright.setup({
+            settings = {
+              python = {
+                analysis = {
+                  autoSearchPaths = true,
+                  useLibraryCodeForTypes = true,
+                },
+              },
+            },
+          })
+        end,
+      }
+    })
+  end,
+},
 })
 
-
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'c',
-    callback = function()
-        vim.keymap.set('n', '<C-h>', ':w<CR>:!gcc % -o out; ./out<CR>', { buffer = true, silent = true })
-        vim.keymap.set('i', '<C-h>', '<Esc>:w<CR>:!gcc % -o out; ./out<CR>', { buffer = true, silent = true })
-    end
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'cpp',
-    callback = function()
-        vim.keymap.set('n', '<C-h>', ':w<CR>:!g++ % -o out; ./out<CR>', { buffer = true, silent = true })
-        vim.keymap.set('i', '<C-h>', '<Esc>:w<CR>:!g++ % -o out; ./out<CR>', { buffer = true, silent = true })
-    end
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'fortran',
-    callback = function()
-        vim.keymap.set('n', '<C-h>', ':w<CR>:!gfortran % -o out; ./out<CR>', { buffer = true, silent = true })
-        vim.keymap.set('i', '<C-h>', '<Esc>:w<CR>:!gfortran % -o out; ./out<CR>', { buffer = true, silent = true })
-    end
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = {'sh', 'go'},
-    callback = function()
-        vim.keymap.set('n', '<C-h>', ':w<CR>:!%<CR>', { buffer = true, silent = true })
-        vim.keymap.set('i', '<C-h>', '<Esc>:w<CR>:!%<CR>', { buffer = true, silent = true })
-    end
-})
-
---
-
-
--- Common keymaps
+-- Общие keymaps (оставить без изменений)
 vim.keymap.set('i', 'jk', '<Esc>', { noremap = true })
 vim.keymap.set('n', ',<Space>', ':nohlsearch<CR>', { noremap = true })
-vim.keymap.set('n', 'H', 'gT', { noremap = true }) -- Переключение вкладок
+vim.keymap.set('n', 'H', 'gT', { noremap = true })
 vim.keymap.set('n', 'L', 'gt', { noremap = true })
 vim.keymap.set('n', ',f', ':Telescope find_files<CR>', { noremap = true })
 vim.keymap.set('n', ',g', ':Telescope live_grep<CR>', { noremap = true })
 vim.keymap.set('n', 'gw', ':bp|bd #<CR>', { noremap = true, silent = true })
 
--- Plugins with packer.nvim
-require('packer').startup(function(use)
-
-    use 'wbthomason/packer.nvim'
-
-    use 'nvim-lua/plenary.nvim' -- For Telescope plugin
-    use 'neovim/nvim-lspconfig' -- LSP
-    use 'hrsh7th/nvim-cmp' -- Autocomplete
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'saadparwaiz1/cmp_luasnip'
-    use 'L3MON4D3/LuaSnip' -- Сниппеты
-    use 'nvim-treesitter/nvim-treesitter' -- Подсветка синтаксиса
-
-    use 'morhetz/gruvbox' -- Color schemes
-    use 'ayu-theme/ayu-vim'
-   --[[
-    use({
-      'rose-pine/neovim',
-      as = 'rose-pine',
-      config = function()
-        require('rose-pine').setup({
-          dark_variant = 'main', -- Variants: 'main', 'moon', 'dawn'
-          disable_background = true, -- Disable background
-          disable_float_background = false, -- Disable background for windows
-        })
-        vim.cmd('colorscheme rose-pine')
-      end,
-    })
-   --]]
-    use 'sainnhe/gruvbox-material'
-    use 'rebelot/kanagawa.nvim'
-
-    -- Comment/uncomment by gcc for current line of gc for seleted lines
-    use {
-      'numToStr/Comment.nvim',
-      config = function()
-        require('Comment').setup({
-            -- Включить/отключить добавление пробела после символа комментария
-            padding = true,
-            -- Переназначаем ключевые привязки
-            toggler = {
-                line = ',cc',  -- Закомментировать строку (вместо 'gcc')
-                block = ',cb', -- Закомментировать блок (вместо 'gbc')
-            },
-            opleader = {
-                line = ',c',   -- Закомментировать строки в визуальном режиме (вместо 'gc')
-                block = ',b',  -- Закомментировать блоки в визуальном режиме (вместо 'gb')
-            },
-        })
-      end
-    }
-
-    use 'nvim-telescope/telescope.nvim'
-    use 'nvim-telescope/telescope-fzf-native.nvim'
-    use 'Pocco81/auto-save.nvim' -- Автосохранение
-    use 'jose-elias-alvarez/null-ls.nvim' -- Форматирование и линтинг
-end)
-
--- Color scheme
--- vim.cmd([[colorscheme "rose-pine-main"]])
-vim.cmd([[colorscheme kanagawa-wave]]) -- kanagawa-wave, kanagawa-dragon, kanagawa-lotus
-
--- LSP
-local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local on_attach = function(client, bufnr)
-
-    -- Быстрые команды для LSP
-    local opts = { buffer = bufnr, noremap = true, silent = true }
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-end
-
--- Настройка LSP для Python (Pyright)
-lspconfig.pyright.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-                diagnosticMode = "workspace",
-            },
-        },
-    },
+-- Автокоманды для запуска кода (оставить без изменений)
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'python',
+  callback = function()
+    vim.opt.colorcolumn = '88'
+    vim.keymap.set({'n', 'i'}, '<C-h>', '<Cmd>w<CR><Cmd>!python3 %<CR>', { buffer = true })
+  end
 })
 
-lspconfig.clangd.setup({})
-
-lspconfig.fortls.setup({})
-
--- Null-ls для Prettier
-require('null-ls').setup({
-    sources = {
-        require('null-ls').builtins.formatting.prettier,
-    }
-
-})
-
--- Telescope
-require('telescope').setup()
-require('telescope').load_extension('fzf')
-
--- Auto-save
-require('auto-save').setup()
-
--- Copy selected text to Windows buffer with "*y
-vim.g.clipboard = {
-  name = 'xsel --clipboard --input',
-  copy = {
-    ['+'] = 'xsel --clipboard --input',
-    ['*'] = 'xsel --clipboard --input',
-  },
-  paste = {
-    ['+'] = 'xsel --clipboard --output',
-    ['*'] = 'xsel --clipboard --output',
-  },
-  cache_enabled = 0,
+local compile_run = {
+  c = 'gcc % -o out && ./out',
+  cpp = 'g++ % -o out && ./out',
+  fortran = 'gfortran % -o out && ./out',
+  sh = '%',
+  go = 'go run %',
 }
 
--- Autocomplete settings
-local cmp = require('cmp')
-local luasnip = require('luasnip')
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  completion = {
-    autocomplete = false, -- Отключить автоматическое появление
-  },
-  mapping = {
-    ['<C-Space>'] = cmp.mapping.complete(), -- Вызов меню автокомплита
-    ['<C-e>'] = cmp.mapping.abort(), -- Закрыть меню
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Подтвердить выбор
-    ['<C-p>'] = cmp.mapping.select_prev_item(), -- Навигация вверх
-    ['<C-n>'] = cmp.mapping.select_next_item(), -- Навигация вниз
-  },
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },  -- Источник из LSP
-        { name = 'luasnip' },  -- Источник для сниппетов
-    }),
-})
+for ft, cmd in pairs(compile_run) do
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = ft,
+    callback = function()
+      vim.keymap.set({'n', 'i'}, '<C-h>', '<Cmd>w<CR><Cmd>!'..cmd..'<CR>', { buffer = true })
+    end
+  })
+end
 
--- transparent bg — remove if you want
+-- Настройки цветовой схемы
+vim.cmd.colorscheme("kanagawa-wave")
+
+-- Прозрачный фон
 vim.cmd([[
   highlight Normal guibg=NONE ctermbg=NONE
   highlight NonText guibg=NONE ctermbg=NONE
   highlight LineNr guibg=NONE ctermbg=NONE
   highlight EndOfBuffer guibg=NONE ctermbg=NONE
 ]])
-
